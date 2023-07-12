119e0f1f6a2a41a5ff06e59785be1f9da7e9e8a4
Jordan Sucher <jsucher@gmail.com>
2023-07-12 18:37:56 -0400

    changed prod dependencies

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,5 @@
 const sequelize = require('sequelize');
 
-// const db = new sequelize(process.env.DATABASE_URL || 'postgres://admin:rvpe7BYW3y6sIORWdm6Q2ihoRfyWFhGE@dpg-cini8n5ph6ei90est0og-a.ohio-postgres.render.com/boilerplate_chkm?ssl=true',{
-//     logging : false
-// })
-
 const db = new sequelize(process.env.DATABASE_URL,{
     logging : false
 })
